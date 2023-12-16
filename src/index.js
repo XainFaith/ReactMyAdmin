e113b9f932e73d82a3107d4a06f1fe4e438bbf2b
Scott Jensen <XainFaith@gmail.com>
2023-12-16 05:44:01 +0000

    Changed id value to PascalCase and imported css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 
 import App from "./App.jsx"
+import "./App.css";
 
-const container = document.getElementById('app');
+const container = document.getElementById('App');
 
 const root = createRoot(container);
 
