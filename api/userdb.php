3fae3a84cf55ff56747ca57e0e97c2dae3e6bdb4
Scott Jensen <XainFaith@gmail.com>
2023-12-18 20:51:16 +0000

    userdb added, used for logged in user authentication

---
+++ api/userdb.php
@@ -0,0 +1,36 @@
+<?php
+include_once "./api.php";
+include_once "./db.php";
+include_once "./tokenValidator.php";
+include_once "./errorReport.php";
+
+$accessPwd = null;
+$tokenUser = null;
+
+if(isset($_POST_DATA['authToken']) == false || isset($_SESSION['user']) == false || isset($_SESSION['encpwd']) == false)
+{
+    session_abort();
+    $report = new ErrorReport();
+    echo $report->stringify();
+    return;
+}
+else
+{
+    $validator = new TokenValidator();
+    $authToken = $_POST_DATA['authToken'];
+    $tokenUser = $_SESSION['user'];
+    $encpwd = $_SESSION['encpwd'];
+    
+    $result = $validator->validateAuthToken($authToken, $tokenUser, $encpwd, $accessPwd);
+
+    //If result does not match the original access token then an error occured
+    if($result != $authToken)
+    {
+        echo $result->stringify();
+        exit();
+    }
+}
+
+
+
+?>
