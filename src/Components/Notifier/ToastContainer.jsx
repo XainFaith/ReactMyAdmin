91bfc95c65406134c52c43d9da6e7f5162125d20
Scott Jensen <XainFaith@gmail.com>
2023-12-18 20:46:19 +0000

    Adding Toast Container

---
+++ src/Components/Notifier/ToastContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function ToastsContainer({children})
+{
+    return(
+        <div className="ToastsContainer">
+            {children}
+        </div>
+    );
+}
