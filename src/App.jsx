c28e00ed52a49c119593f34f1f4a20a4038702bc
Scott Jensen <XainFaith@gmail.com>
2023-12-18 20:49:16 +0000

    Style Additions and Toasts components in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import React from "react";
-import ModalPanel from "./Components/UI/ModalPanel";
-import LoginPanel from "./Components/LoginPanel";
+import React, { createContext, useState } from 'react';
+import LoginPanel from './Components/LoginPanel';
+import Notifier from './Components/Notifier/Notifier';
+
+import { UserAccessContext, user } from './Components/AuthUser';
+import DatabasePanel from './Components/DatabasePanel';
 
 export default function App()
 {
-    function onAccessTokenGranted()
-    {
 
+    const [authUser, setAuthUser] = useState(user);
+
+    function onGranted(token)
+    {
+        setAuthUser({...authUser, authToken: token});
     }
 
-    return(<LoginPanel onAccessTokenGranted={onAccessTokenGranted}/>);
+    return( <Notifier> 
+            { authUser.authToken ? 
+                <UserAccessContext.Provider  value={authUser}> 
+                    <div className='WorkSpace'>
+                        <DatabasePanel />
+                    </div>
+                </UserAccessContext.Provider>
+                :
+                <LoginPanel onGranted={onGranted}/>
+            }
+            </Notifier>);
+
 }
