e429067a3622c34f57229aa3d668cd25f047f964
Scott Jensen <XainFaith@gmail.com>
2023-12-20 08:07:01 +0000

    Updated to Use Resizeable Panels and new Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { createContext, useState } from 'react';
 import LoginPanel from './Components/LoginPanel';
 import Notifier from './Components/Notifier/Notifier';
-
+import ResizeablePanel from './Components/UI/ResizeablePanels/ResizeablePanel';
+import ResizeHandle from './Components/UI/ResizeablePanels/ResizeHandle';
 import { UserAccessContext, user } from './Components/AuthUser';
 import DatabasePanel from './Components/DatabasePanel';
+import Sidebar from './Components/SideBar';
 
 export default function App()
 {
@@ -19,7 +21,14 @@ export default function App()
             { authUser.authToken ? 
                 <UserAccessContext.Provider  value={authUser}> 
                     <div className='WorkSpace'>
-                        <DatabasePanel />
+                        <Sidebar />
+                        <div className="SubSpace">
+                            <ResizeablePanel isVertical={true} defaultSize="70%">
+                            </ResizeablePanel>
+                            <ResizeHandle isVertical={true}/>
+                            <ResizeablePanel isVertical={true} defaultSize="30%">
+                            </ResizeablePanel>
+                        </div>
                     </div>
                 </UserAccessContext.Provider>
                 :
