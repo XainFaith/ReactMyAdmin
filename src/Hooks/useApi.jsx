95eb1df95f91de8c1fe6c781b6fc7ab7acab29cc
Scott Jensen <XainFaith@gmail.com>
2023-12-21 07:21:27 +0000

    Added useApi React hook

---
+++ src/Hooks/useApi.jsx
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+import { NotifierContext } from "../Components/Notifier/Notifier";
+import { UserAccessContext } from "../Components/AuthUser";
+
+export default function useApi(url)
+{
+    const notifier = useContext(NotifierContext);
+    const authUser = useContext(UserAccessContext);
+
+    const [error, setError] = useState(null);
+    const [response, setResponse] = useState(null);
+    const [awaiting, setAwaiting] = useState(false);
+
+    function callApi(data)
+    {
+        setAwaiting(true);
+        const postData = {...data, 'authToken': authUser.authToken};
+
+        axios.post('./api/' + url,postData).then((resp)=>
+        {
+            setAwaiting(false);
+            if(resp.data.errorCode !== undefined)
+            {
+                //Code 1001 is an auth error user no longer is valid 
+                if(resp.data.errorCode == 1001)
+                {
+                    authUser.onTokenExipre();
+                }
+                
+                notifier.createError(resp.data.errorMessage);
+                setError(resp.data.errorCode);
+                return;
+            }
+    
+            setResponse(resp.data);
+        }).catch((error)=>
+        {
+            setAwaiting(false);
+            notifier.createError(error.message);
+        })
+        
+    }
+
+    return [response, error, awaiting, callApi];
+}
