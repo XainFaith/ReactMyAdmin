ddb9fc5a4b2364e2b55d9d7c95883ec95f78f903
Scott Jensen <XainFaith@gmail.com>
2023-12-21 15:57:11 +0000

    Implemented basic logout

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import CollapsiblePanel from "./UI/CollapsiblePanel";
-import IconButton from "./UI/IconButton";
 import DatabasePanel from "./DatabasePanel";
+import useApi from "../Hooks/useApi";
 
 import "./SideBar.css";
 
@@ -10,6 +10,7 @@ export default function Sidebar()
 {
 
     const [visiblePanel, setVisiblePanel] = useState("Databases");
+    const [callApi] = useApi('logout.php');
 
     const onButtonClick = (panelName)=>
     {
@@ -25,7 +26,7 @@ export default function Sidebar()
 
     const onPromptLogout = ()=>
     {
-
+        callApi();
     }
 
     return(
@@ -34,7 +35,7 @@ export default function Sidebar()
                 <input type="checkbox" className="DatabaseIcon" onChange={()=>onButtonClick("Databases")} checked={visiblePanel == "Databases"} />
                 <input type="checkbox" className="TablesIcon" onChange={()=>onButtonClick("Tables")} checked={visiblePanel == "Tables"} />
                 <input type="checkbox" className="SettingsIcon BottomOfSideBar" onChange={()=>onButtonClick("Settings")} checked={visiblePanel == "Settings"} />
-                <input type="checkbox" className="LogoutIcon" onChange={onPromptLogout} checked={false} />
+                <button className="LogoutIcon" onClick={onPromptLogout} />
             </div>
             <CollapsiblePanel isCollpased={(visiblePanel != "Databases")}>
                 <DatabasePanel />
