982237eb827ab6e1244e7988066dc16eeeb6248e
Scott Jensen <XainFaith@gmail.com>
2023-12-21 18:07:29 +0000

    Added Tables Panel component and api calls to get table names of selected database

--- src/Components/TablesPanel.jsx
+++ src/Components/TablesPanel.jsx
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
-
+import React, { useContext, useEffect } from "react";
 import useApi from "../Hooks/useApi";
+import LoadingOverlay from "./UI/LoadingOverlay";
+import NavList from "./UI/NavList";
+
+import { UserAccessContext } from "./AuthUser";
+import "./TablesPanel.css";
 
 export default function TablesPanel()
 {
-    const [tableNames, setTableNames] = useState([]);
+    const authUser = useContext(UserAccessContext);
+    const [callApi, response, error, awaiting] = useApi('getTableNames.php');
 
-    const [callApi, response, error, awaiting] = useApi('getTables.php');
+    useEffect(()=>
+    {
+        if(authUser.selectedDatabase != null)
+        {
+            callApi();    
+        }  
+    }
+    ,[authUser.selectedDatabase]);
+
+    const onTableSelected = (name)=>
+    {
+
+    }
 
     return(
-     null
+        <div className="TablesPanel">
+        <LoadingOverlay isActive={awaiting}>
+        <div className="TablesPanelHeader">Tables:</div>
+        <div className="TablesNavContainer">
+            {(response && error == null )? <NavList navElements={response.tables} className="TablesNavList" onItemSelected={onTableSelected}/> : "No Tables Avaliable"}
+        </div>
+        </LoadingOverlay>
+    </div>
     );
 }
