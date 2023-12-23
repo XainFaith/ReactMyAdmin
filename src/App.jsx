d2d97d5760e84fc082325015e41d8618629c947b
Scott Jensen <XainFaith@gmail.com>
2023-12-23 07:05:26 +0000

    extracted context code of auth user

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
-import LoginPanel from './Components/LoginPanel';
+import UserAccessProvider from './Context/AuthUser';
 import Notifier from './Components/Notifier/Notifier';
-import ResizeablePanel from './Components/UI/ResizeablePanels/ResizeablePanel';
-import ResizeHandle from './Components/UI/ResizeablePanels/ResizeHandle';
-import Sidebar from './Components/SideBar/SideBar';
-
-import { UserAccessContext, user } from './Context/AuthUser';
+import UserArea from './UserArea';
+import NonUserArea from './NonUserArea';
 
 import "./App.css";
-import Workspace from './Components/Workspace/Workspace';
 
 export default function App()
 {
 
-    const [authUser, setAuthUser] = useState(user);
-
-    function onGranted(token)
-    {
-        setAuthUser({...authUser, authToken: token, logout: onLogout});
-    }
-
-    function onLogout()
-    {
-        setAuthUser({...authUser, authToken: null});
-    }
-
     return( <Notifier> 
-            { authUser.authToken ? 
-                <UserAccessContext.Provider  value={authUser}> 
-                    <div className='WorkSpace'>
-                        <Sidebar />
-                        <div className="SubSpace">
-                            <ResizeablePanel isVertical={true} defaultSize="70%">
-                                <Workspace />
-                            </ResizeablePanel>
-                            <ResizeHandle isVertical={true}/>
-                            <ResizeablePanel isVertical={true} defaultSize="30%">
-                            </ResizeablePanel>
-                        </div>
-                    </div>
-                </UserAccessContext.Provider>
-                :
-                <LoginPanel onGranted={onGranted}/>
-            }
+            <UserAccessProvider>
+                <UserArea />
+                <NonUserArea />
+            </UserAccessProvider>
             </Notifier>);
 
 }
