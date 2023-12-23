4cf63bb755b26a741ca4b41a22965f998af1b79b
Scott Jensen <XainFaith@gmail.com>
2023-12-23 07:07:09 +0000

    Shifted tab order so structure is the first and default tab selected

--- src/Components/Workspace/Workspace.jsx
+++ src/Components/Workspace/Workspace.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import TabContainer from "../UI/Tab/TabContainer"
 import Tab from "../UI/Tab/Tab";
+import StructureViewer from "./StructureViewer";
 
 export default function Workspace()
 {
 
     return(
-        <TabContainer defaultTab="Browse">
-            <Tab name="Browse">
-
-            </Tab>
+        <TabContainer defaultTab="Structure">
             <Tab name="Structure">
+                <StructureViewer />
+            </Tab>
+            <Tab name="Browse">
 
             </Tab>
             <Tab name="SQL">
