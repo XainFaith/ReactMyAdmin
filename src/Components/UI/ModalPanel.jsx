720cd6f257aef23964330c76d38641ea0f2d71c7
Scott Jensen <XainFaith@gmail.com>
2023-12-25 07:30:02 +0000

    Added className pass through for modals

--- src/Components/UI/ModalPanel.jsx
+++ src/Components/UI/ModalPanel.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import FieldSet from "./FieldSet";
 import "./ModalPanel.css";
 
-export default function ModalPanel({children, Title, isVisible=false})
+export default function ModalPanel({children, Title, isVisible=false, className})
 {
     if(isVisible == false) return;
 
     return( 
             <div className="Overlay">
-                <div className="ModalContainer">
+                <div className={className ? "ModalContainer " + className : "ModalContainer"}>
                     <FieldSet Title={Title}>
                         {children}
                     </FieldSet>
