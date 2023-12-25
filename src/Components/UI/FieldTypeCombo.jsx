a8f4411d10d91504975eeae0623df6618b3f9691
Scott Jensen <XainFaith@gmail.com>
2023-12-25 07:30:43 +0000

    Added Filed Type Combobox component

--- src/Components/UI/FieldTypeCombo.jsx
+++ src/Components/UI/FieldTypeCombo.jsx
@@ -6,63 +6,63 @@ export default function FieldTypeCombo({onFieldTypeChange, defaultField})
 
     return(
         <select defaultValue={defaultField} onChange={()=>{onFieldTypeChange(e.target.value)}}>
-            <option>INT</option>
-            <option>VARCHAR</option>
-            <option>TEXT</option>
-            <option>DATE</option>
+            <option value="int">INT</option>
+            <option value="varchar">VARCHAR</option>
+            <option value="text">TEXT</option>
+            <option value="date">DATE</option>
             <optgroup label="Numeric">
-                <option>TINYINT</option>
-                <option>SMALLINT</option>
-                <option>MEDIUMINT</option>
-                <option>INT</option>
-                <option>BIGINT</option>
-                <option>DECIMAL</option>
-                <option>FLOAT</option>
-                <option>DOUBLE</option>
-                <option>REAL</option>
-                <option>BIT</option>
-                <option>BOOLEAN</option>
-                <option>SERIAL</option>
+                <option value="tinyint">TINYINT</option>
+                <option value="smallint">SMALLINT</option>
+                <option value="mediumint">MEDIUMINT</option>
+                <option value="int">INT</option>
+                <option value="bigint">BIGINT</option>
+                <option value="decimal">DECIMAL</option>
+                <option value="float">FLOAT</option>
+                <option value="double">DOUBLE</option>
+                <option value="real">REAL</option>
+                <option value="bit">BIT</option>
+                <option value="boolean">BOOLEAN</option>
+                <option value="serial">SERIAL</option>
             </optgroup>
             
             <optgroup label="Data and Time">
-                <option>DATE</option>
-                <option>DATETIME</option>
-                <option>TIMESTAMP</option>
-                <option>TIME</option>
-                <option>YEAR</option>
+                <option value="date">DATE</option>
+                <option value="datetime">DATETIME</option>
+                <option value="timestamp">TIMESTAMP</option>
+                <option value="time">TIME</option>
+                <option value="year">YEAR</option>
             </optgroup>
 
             <optgroup label="String">
-                <option>CHAR</option>
-                <option>VARCHAR</option>
-                <option>TINYTEXT</option>
-                <option>TEXT</option>
-                <option>MEDIUMTEXT</option>
-                <option>LONGTEXT</option>
-                <option>BINARY</option>
-                <option>VARBINARY</option>
-                <option>TINYBLOB</option>
-                <option>MEDIUMBLOB</option>
-                <option>BLOB</option>
-                <option>LONGBLOB</option>
-                <option>ENUM</option>
-                <option>SET</option>
+                <option value="char">CHAR</option>
+                <option value="varchar">VARCHAR</option>
+                <option value="tinytext">TINYTEXT</option>
+                <option value="text">TEXT</option>
+                <option value="mediumtext">MEDIUMTEXT</option>
+                <option value="longtext">LONGTEXT</option>
+                <option value="binary">BINARY</option>
+                <option value="varbinary">VARBINARY</option>
+                <option value="tinyblob">TINYBLOB</option>
+                <option value="mediumblob">MEDIUMBLOB</option>
+                <option value="blob">BLOB</option>
+                <option value="longblob">LONGBLOB</option>
+                <option value="enum">ENUM</option>
+                <option value="set">SET</option>
             </optgroup>
 
             <optgroup label="Spatial">
-                <option>GEOMETRY</option>
-                <option>POINT</option>
-                <option>LINESTRING</option>
-                <option>POLYGON</option>
-                <option>MULTIPOINT</option>
-                <option>MULTILNESTRING</option>
-                <option>MULTIPOLYGON</option>
-                <option>GEMETRYCOLLECTION</option>
+                <option value="geometry">GEOMETRY</option>
+                <option value="point">POINT</option>
+                <option value="linestring">LINESTRING</option>
+                <option value="polygon">POLYGON</option>
+                <option value="multipoint">MULTIPOINT</option>
+                <option value="multilinestring">MULTILNESTRING</option>
+                <option value="multipolygon">MULTIPOLYGON</option>
+                <option value="geometrycollection">GEMETRYCOLLECTION</option>
             </optgroup>
 
             <optgroup label="JSON">
-                <option>JSON</option>
+                <option  value="json">JSON</option>
             </optgroup>
 
         </select>
